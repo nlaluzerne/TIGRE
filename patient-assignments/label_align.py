659315d746bf10ef025578fb58d16b5a1428b360
Carter Maxcy <cartermaxcy@ku.edu>
2019-02-05 16:27:55 -0600

    Extended test and completed code

--- patient-assignments/label_align.py
+++ patient-assignments/label_align.py
@@ -1,4 +1,17 @@
 import pandas as pd
+from collections import defaultdict
 
 def align(df):
-    return None  # TEMP
+    out_dict = defaultdict(list)
+    for patient_name, group in df.groupby('patient_id'):
+        out_dict['patient_id'].append(patient_name)
+        L_1, L_2 = group['L']
+        R_1, R_2 = group['R']
+        name_1, name_2 = group['name']
+        out_dict['L_1'].append(L_1)
+        out_dict['L_2'].append(L_2)
+        out_dict['R_1'].append(R_1)
+        out_dict['R_2'].append(R_2)
+        out_dict['name_1'].append(name_1)
+        out_dict['name_2'].append(name_2)
+    return pd.DataFrame(out_dict).reset_index(drop=True)
--- patient-assignments/label_align_test.py
+++ patient-assignments/label_align_test.py
@@ -6,19 +6,22 @@ class TestLabelAlign(unittest.TestCase):
 
     def test_align(self):
         test_labels = pd.DataFrame()
-        test_labels['name'] = ['Bob', 'Jane']
-        test_labels['patient_id'] = ['sample_id', 'sample_id']
-        test_labels['L'] = ['Y', 'N']
-        test_labels['R'] = ['N', 'Y']
+        test_labels['name'] = ['Bob', 'Jane', 'Jane', 'Paul']
+        test_labels['patient_id'] = ['sample_id_1', 'sample_id_1',
+            'sample_id_2', 'sample_id_2']
+        test_labels['L'] = ['Y', 'N', 'N', 'Y']
+        test_labels['R'] = ['N', 'Y', 'Y', 'N']
 
         expected_aligned = pd.DataFrame()
-        expected_aligned['name_1'] = ['Bob']
-        expected_aligned['name_2'] = ['Jane']
-        expected_aligned['patient_id'] = ['sample_id']
-        expected_aligned['L_1'] = ['Y']
-        expected_aligned['L_2'] = ['N']
-        expected_aligned['R_1'] = ['N']
-        expected_aligned['R_2'] = ['Y']
+        expected_aligned['name_1'] = ['Bob', 'Jane']
+        expected_aligned['name_2'] = ['Jane', 'Paul']
+        expected_aligned['patient_id'] = ['sample_id_1', 'sample_id_2']
+        expected_aligned['L_1'] = ['Y', 'N']
+        expected_aligned['L_2'] = ['N', 'Y']
+        expected_aligned['R_1'] = ['N', 'Y']
+        expected_aligned['R_2'] = ['Y', 'N']
+        expected_aligned = expected_aligned[['L_1', 'L_2', 'R_1', 'R_2',
+            'name_1', 'name_2', 'patient_id']]
 
         actual_aligned = align(test_labels)
         pd.testing.assert_frame_equal(expected_aligned, actual_aligned)

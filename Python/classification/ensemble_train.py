e245c91b54cc63090fe69d3c62dfe84e3849bfa7
Carter Maxcy <cartermaxcy@ku.edu>
2019-04-20 10:19:44 -0500

    Fixed patient reading bug

--- Python/classification/ensemble_train.py
+++ Python/classification/ensemble_train.py
@@ -3,8 +3,12 @@ from feature_extract import all_f
 from sklearn.svm import SVC
 import numpy as np
 
-N = os.listdir('Images_noBG/N')
-Y = os.listdir('Images_noBG/Y')
+def file_lines(file_name):
+    with open(file_name) as f:
+        return [line[:-1] for line in f]
+
+N = file_lines('patientAssignments/patients_nocancer.txt')
+Y = file_lines('patientAssignments/patients_cancer.txt')
 
 N_mat = [all_f(patient + 'A2BA-f.jpg', patient + 'A2BA-fc.jpg') for patient in N]
 Y_mat = [all_f(patient + 'A2BA-f.jpg', patient + 'A2BA-fc.jpg') for patient in Y]

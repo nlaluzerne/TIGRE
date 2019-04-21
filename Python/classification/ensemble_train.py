Scaling bug appears to be fixed

Python/classification/ensemble_train.py
@@ -32,17 +32,20 @@ f_suffix = 'A2BA-f.jpg'
 fc_suffix = 'A2BA-fc.jpg'
 
 N_mat = [all_f(read_img(N_path + patient + f_suffix), read_img(N_path + patient + fc_suffix)) for patient in N]
-Y_mat = [all_f(read_img(Y_path + patient + f_suffix), read_img(Y_path + patient + f_suffix)) for patient in Y]
+Y_mat = [all_f(read_img(Y_path + patient + f_suffix), read_img(Y_path + patient + fc_suffix)) for patient in Y]
 N_labels = [0] * len(N_mat)
 Y_labels = [1] * len(Y_mat)
 
 X = np.array(N_mat + Y_mat)
 y = np.array(N_labels + Y_labels)
-X = StandardScaler().fit_transform(X)
+
+scaler = StandardScaler()
+
+X = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-model = SVC(C=10, gamma=.001)
+model = SVC(C=10, gamma=.001, probability=True)
 model.fit(X_train, y_train)
 
 print(model.score(X_test, y_test))